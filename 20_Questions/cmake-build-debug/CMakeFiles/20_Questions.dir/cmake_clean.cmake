be7b618ced680080716e92fd2770d1765a62495d
Aidan Lamkin <93846375+Aidan-Lamkin@users.noreply.github.com>
2021-11-07 10:08:02 -0700

    Add files via upload

---
+++ 20_Questions/cmake-build-debug/CMakeFiles/20_Questions.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "20_Questions"
+  "20_Questions.pdb"
+  "CMakeFiles/20_Questions.dir/animal.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/20_Questions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
